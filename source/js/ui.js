cd7b3b7e9a4260e03f8d435b3eec5e7ed0d0e133
Yevgeny <unstoo@gmail.com>
2018-05-13 16:03:53 +0300

    mobile menu toggling

---
+++ source/js/ui.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const MENU_CLASS_NAME = 'main-nav__list--hidden',
+    TOGGLER_CLASS_NAME = 'main-nav__toggle--closed',
+    MENU_ITEM_CLASS_NAME = 'main-nav__link--no-tabindex'
+
+const menuToggler = document.getElementById('menu-toggler'),
+    menu = document.getElementById('main-nav'),
+    menuItems = Array.from(document.getElementsByClassName('main-nav__link'))
+
+
+
+if (window.innerWidth < 768) {
+    // Меню спрятано. Переключение по пунктам меню табом не нужно.
+    menuItems.forEach(item => item.setAttribute('tabindex', '-1'))
+    initToggler()
+} else {
+    // Показать скрытое меню.
+    menu.classList.remove(MENU_CLASS_NAME)
+}   
+
+
+
+function initToggler() {
+    menuToggler.addEventListener('click', () => {
+        
+        menu.classList.value.includes(MENU_CLASS_NAME) ?
+        (() => {            
+            menu.classList.remove(MENU_CLASS_NAME)
+            menuToggler.classList.remove(TOGGLER_CLASS_NAME)
+            menuItems.forEach(item => item.setAttribute('tabindex', '0'))
+        })()
+        : 
+        (() => {
+            menu.classList.add(MENU_CLASS_NAME)
+            menuToggler.classList.add(TOGGLER_CLASS_NAME)
+            menuItems.forEach(item => item.setAttribute('tabindex', '-1'))
+        })()
+    })
+}
